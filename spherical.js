62a4bb1340d866d17004f8df1602712dd3d08d83
goriliukasbuxton <laurynasgedminas@gmail.com>
2016-01-21 21:15:46 -0500

    files

---
+++ spherical.js
@@ -0,0 +1,27 @@
+﻿define([
+    'dojo/_base/declare',
+    'dijit/_WidgetBase',
+    'gis/lib3/latlon-spherical',
+    'gis/lib3/dms',
+], function (
+    declare,
+    _WidgetBase,
+    LatLon, Dms
+) {
+
+    return declare([_WidgetBase], {
+
+        postCreate: function () {
+            this.inherited(arguments);
+            a = new LatLon(27.165, -80.16);
+            console.log(a);
+        },
+
+        startup: function () {
+
+        },
+
+
+    })
+
+});
